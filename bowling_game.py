fef6018c0bce995779b1997570581c15fe684052
Tom Elliff <tomelliff@gmail.com>
2016-03-27 19:50:51 +0000

    PEP8 style changes

--- bowling_game.py
+++ bowling_game.py
@@ -1,40 +1,40 @@
 #!/usr/bin/env python
 
 class Game:
-  def __init__(self):
-    self._rolls = [0] * 21
-    self._current_roll = 0
+    def __init__(self):
+        self._rolls = [0] * 21
+        self._current_roll = 0
 
-  def roll(self, pins):
-    self._rolls[self._current_roll] = pins
-    self._current_roll += 1
+    def roll(self, pins):
+        self._rolls[self._current_roll] = pins
+        self._current_roll += 1
 
-  def score(self):
-    score = 0
-    frame_index = 0
-    for frame in range(0, 10):
-      if self._is_strike(frame_index):
-        score += 10 + self._strike_bonus(frame_index)
-        frame_index += 1
-      elif self._is_spare(frame_index):
-        score += 10 + self._spare_bonus(frame_index)
-        frame_index += 2
-      else:
-        score += self._rolls[frame_index] + self._rolls[frame_index + 1]
-        frame_index += 2
-    return score
+    def score(self):
+        score = 0
+        frame_index = 0
+        for frame in range(0, 10):
+            if self._is_strike(frame_index):
+                score += 10 + self._strike_bonus(frame_index)
+                frame_index += 1
+            elif self._is_spare(frame_index):
+                score += 10 + self._spare_bonus(frame_index)
+                frame_index += 2
+            else:
+                score += self._rolls[frame_index] + self._rolls[frame_index + 1]
+                frame_index += 2
+        return score
 
-  def _sum_of_balls_in_frame(self, frame_index):
-    return self._rolls[frame_index] + self._rolls[frame_index + 1]
+    def _sum_of_balls_in_frame(self, frame_index):
+        return self._rolls[frame_index] + self._rolls[frame_index + 1]
 
-  def _spare_bonus(self, frame_index):
-    return self._rolls[frame_index + 2]
+    def _spare_bonus(self, frame_index):
+        return self._rolls[frame_index + 2]
 
-  def _strike_bonus(self, frame_index):
-    return self._rolls[frame_index + 1] + self._rolls[frame_index + 2]
+    def _strike_bonus(self, frame_index):
+        return self._rolls[frame_index + 1] + self._rolls[frame_index + 2]
 
-  def _is_spare(self, frame_index):
-    return self._rolls[frame_index] + self._rolls[frame_index + 1] == 10
+    def _is_spare(self, frame_index):
+        return self._rolls[frame_index] + self._rolls[frame_index + 1] == 10
 
-  def _is_strike(self, frame_index):
-    return self._rolls[frame_index] == 10
+    def _is_strike(self, frame_index):
+        return self._rolls[frame_index] == 10
--- bowling_game_test.py
+++ bowling_game_test.py
@@ -5,49 +5,48 @@ import unittest
 from bowling_game import Game
 
 class BowlingGameTest(unittest.TestCase):
+    def setUp(self):
+        self.g = Game()
 
-  def setUp(self):
-    self.g = Game()
+    def roll_many(self, rolls, pins):
+        for roll in range(0, rolls):
+            self.g.roll(pins)
 
-  def roll_many(self, rolls, pins):
-    for roll in range(0, rolls):
-      self.g.roll(pins)
+    def roll_spare(self):
+        self.g.roll(5)
+        self.g.roll(5)
 
-  def roll_spare(self):
-    self.g.roll(5)
-    self.g.roll(5)
+    def roll_strike(self):
+        self.g.roll(10)
 
-  def roll_strike(self):
-    self.g.roll(10)
+    def test_gutter_game(self):
+        rolls = 20
+        pins = 0
+        self.roll_many(rolls, pins)
+        self.assertEquals(self.g.score(), 0)
 
-  def test_gutter_game(self):
-    rolls = 20
-    pins = 0
-    self.roll_many(rolls, pins)
-    self.assertEquals(self.g.score(),0)
+    def test_all_ones(self):
+        rolls = 20
+        pins = 1
+        self.roll_many(rolls, pins)
+        self.assertEquals(self.g.score(), 20)
 
-  def test_all_ones(self):
-    rolls = 20
-    pins = 1
-    self.roll_many(rolls, pins)
-    self.assertEquals(self.g.score(),20)
+    def test_one_spare(self):
+        self.roll_spare()
+        self.g.roll(3)
+        self.roll_many(17, 0)
+        self.assertEquals(self.g.score(), 16)
 
-  def test_one_spare(self):
-    self.roll_spare()
-    self.g.roll(3)
-    self.roll_many(17, 0)
-    self.assertEquals(self.g.score(),16)
+    def test_one_strike(self):
+        self.roll_strike()
+        self.g.roll(3)
+        self.g.roll(4)
+        self.roll_many(16, 0)
+        self.assertEquals(self.g.score(), 24)
 
-  def test_one_strike(self):
-    self.roll_strike()
-    self.g.roll(3)
-    self.g.roll(4)
-    self.roll_many(16, 0)
-    self.assertEquals(self.g.score(),24)
-
-  def test_perfect_game(self):
-    self.roll_many(12, 10)
-    self.assertEquals(self.g.score(),300)
+    def test_perfect_game(self):
+        self.roll_many(12, 10)
+        self.assertEquals(self.g.score(), 300)
 
 if __name__ == '__main__':
     unittest.main()
