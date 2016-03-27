8555fabf673a5a99968b302178f3967b1bccef11
Tom Elliff <tomelliff@gmail.com>
2016-03-27 13:49:12 +0000

    Refactor tests with helper function

--- bowling_game_test.py
+++ bowling_game_test.py
@@ -6,18 +6,24 @@ from bowling_game import Game
 
 class BowlingGameTest(unittest.TestCase):
 
+  def setUp(self):
+    self.g = Game()
+
+  def rollMany(self, rolls, pins):
+    for roll in range(0, rolls):
+      self.g.roll(pins)
 
   def test_gutter_game(self):
-    g = Game()
-    for roll in range(0,20):
-      g.roll(0)
-    self.assertEquals(g.score(),0)
+    rolls = 20
+    pins = 0
+    self.rollMany(rolls, pins)
+    self.assertEquals(self.g.score(),0)
 
   def test_all_ones(self):
-    g = Game()
-    for roll in range(0,20):
-      g.roll(1)
-    self.assertEquals(g.score(),20)
+    rolls = 20
+    pins = 1
+    self.rollMany(rolls, pins)
+    self.assertEquals(self.g.score(),20)
 
 if __name__ == '__main__':
     unittest.main()
