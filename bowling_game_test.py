7d76fc895716e7aa8d4895c50953766ae3e45e25
Tom Elliff <tomelliff@gmail.com>
2016-03-28 20:49:16 +0000

    Create generic roll helper function for testing
    Takes a list of rolls with each item being the pins knocked down.

--- bowling_game_test.py
+++ bowling_game_test.py
@@ -8,8 +8,8 @@ class BowlingGameTest(unittest.TestCase):
     def setUp(self):
         self.g = Game()
 
-    def roll_many(self, rolls, pins):
-        for roll in range(0, rolls):
+    def roll(self, rolls):
+        for pins in rolls:
             self.g.roll(pins)
 
     def roll_spare(self):
@@ -22,30 +22,27 @@ class BowlingGameTest(unittest.TestCase):
     def test_gutter_game(self):
         rolls = 20
         pins = 0
-        self.roll_many(rolls, pins)
+        self.roll([pins] * rolls)
         self.assertEqual(self.g.score(), 0)
 
     def test_all_ones(self):
         rolls = 20
         pins = 1
-        self.roll_many(rolls, pins)
+        self.roll([pins] * rolls)
         self.assertEqual(self.g.score(), 20)
 
     def test_one_spare(self):
         self.roll_spare()
-        self.g.roll(3)
-        self.roll_many(17, 0)
+        self.roll([3] + [0] * 17)
         self.assertEqual(self.g.score(), 16)
 
     def test_one_strike(self):
         self.roll_strike()
-        self.g.roll(3)
-        self.g.roll(4)
-        self.roll_many(16, 0)
+        self.roll([3,4] + [0] * 16)
         self.assertEqual(self.g.score(), 24)
 
     def test_perfect_game(self):
-        self.roll_many(12, 10)
+        self.roll([10] * 12)
         self.assertEqual(self.g.score(), 300)
 
 if __name__ == '__main__':
